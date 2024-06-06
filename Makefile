f73841ca8fd47fceca8e3e2658e61abfedc167c3
pixie <pixie.pixie@gmail.com>
2024-06-06 12:03:49 +0300

    design stage

--- Makefile
+++ Makefile
@@ -3,3 +3,7 @@
 
 TARGET=./bin
 CC = gcc -std=c++17
+
+#build
+#link
+#clean
--- sniffer.cpp
+++ sniffer.cpp
@@ -2,5 +2,15 @@
 
 int main() {
 
+// create raw socket
+
+// use raw socket to loop through all the trafic
+
+// get info on screen real-time
+
+// show trafic summary
+
+// exit option
+
     return 0;
 }
