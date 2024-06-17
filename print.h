e05c2a80f01466bef01e337c2898ce84187387f1
colouredpixie <pixie.pixie@gmail.com>
2024-06-17 16:57:31 +0300

    Functional update

--- print.h
+++ print.h
@@ -1,46 +1,51 @@
 #pragma once
 
-
 #include <iostream>
-#include <cstring>
+#include <iomanip>
+#include <vector>
 #include <netinet/ip.h>
 #include <netinet/if_ether.h>
 #include <arpa/inet.h>
 
+namespace print {
+    #define STYLE(text) std::left << std::setfill(' ') << std::setw(30) << text << std::right 
 
-// incoming packets
-// outgoing packets
-
-namespace printout {
     std::string baseColor = "\033[0m";
-    std::string headerColor = "\033[36m";
-    std::string totalColor = "\033[32m";
+    std::string standardColor = "\033[92;1m";
+    std::string incomingColor = "\033[36;1m";    
+    std::string totalColor = "\033[93;1m";
+    std::string currentColor;
 
-void printInfo(const struct ether_header *eth, const struct iphdr *ip) {
-    struct sockaddr_in source, dest;
-    memset(&source, 0, sizeof(source));
-    source.sin_addr.s_addr = ip->saddr;
-    memset(&dest, 0, sizeof(dest));
-    dest.sin_addr.s_addr = ip->daddr;
+    // Can be filled with local IP addresses dynamically
+    std::vector<std::string> IPs{"127.0.0.1","192.168.88.255"};
 
-    std::string color = headerColor;
-    std::string address = inet_ntoa(dest.sin_addr) ;
-    if (address == "127.0.0.1") {
-        color = "\033[35m";
-        std::cout << color << "Incoming packet:" << baseColor << std::endl;
-    } else {
-        std::cout << color << "Outgoing packet:" << baseColor << std::endl;
+    bool isIncomingPacket(std::string address) {
+        for (std::string IP : IPs) {
+            if (address == IP) { return true; }
+        }
+        return false;
     }
-    std::cout << color << "Size of Packet   : " << baseColor << ntohs(ip->tot_len) << std::endl;
-    std::cout << color << "Source           : " << baseColor << inet_ntoa(source.sin_addr) << std::endl;
-    std::cout << color << "Destination      : " << baseColor << inet_ntoa(dest.sin_addr) << std::endl;
-}
 
-// summary
-void printSum(int count, int sum)  {
-    std::cout << "___________________________________" << std::endl << std::endl;
-    std::cout << "Packets caught   : " <<  count << std::endl;
-    std::cout << "Total bytes      : " <<  sum << std::endl;
-    std::cout << "___________________________________" << std::endl << std::endl;
-}
+    // Prints packet header info
+    void printInfo(const std::pair<struct ether_header *, struct iphdr *> info) {
+        struct sockaddr_in source{0}, dest{0};
+        source.sin_addr.s_addr = info.second->saddr;
+        dest.sin_addr.s_addr = info.second->daddr;
+        
+        if (isIncomingPacket(inet_ntoa(dest.sin_addr))) { currentColor = incomingColor; } 
+            else { currentColor = standardColor; }
+
+        std::cout << "Packet analysis:" << std::endl;
+        std::cout << currentColor << STYLE("Size") << baseColor << ntohs(info.second->tot_len) << std::endl;
+        std::cout << currentColor << STYLE("Source") << baseColor << inet_ntoa(source.sin_addr) << std::endl;
+        std::cout << currentColor << STYLE("Destination") << baseColor << inet_ntoa(dest.sin_addr) << std::endl;
+    }
+
+    // Prints global summary
+    void printSum(int count, int sum)  {
+        std::cout << std::setfill('-') << std::setw(50) << baseColor << std::endl;
+        std::cout << totalColor << STYLE("Total packets caught") <<  baseColor << count << std::endl;
+        std::cout << totalColor << STYLE("Total bytes") <<  baseColor << sum << std::endl;
+        std::cout << totalColor << std::setfill('-') << std::setw(50) << baseColor << std::endl << std::endl;
+    }
 }
